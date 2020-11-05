a0ecdaf5c339385f5a3baaef7173643b3251d45d
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 15:58:05 -0800

    Add leap year exercise solution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Simplified version
+    return (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
 
+    // Tenary expression above in if/else statements
+    // if (year % 100 == 0) {
+    //     if (year % 400 == 0) return true;
+    // } else {
+    //     if (year % 4 == 0) return true;
+    // }
+    // return false;
 }
 
 module.exports = leapYears
