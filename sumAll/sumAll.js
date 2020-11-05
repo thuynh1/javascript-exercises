830cb2c38c391ac63cfb21b4c5addb3a462df880
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 15:14:47 -0800

    Add sum all exercise solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(limit1, limit2) {
+    if (!Number.isInteger(limit1) ||
+        !Number.isInteger(limit2) ||
+        limit1 < 0 || limit2 < 0) return "ERROR";
 
+    let start;
+    let end;
+    let result = 0;
+
+    [start, end] = (limit1 < limit2) ? [limit1, limit2] : [limit2, limit1];
+
+    for (let value = start; value <= end; value += 1) {
+        result += value;
+    }
+    return result;
 }
 
 module.exports = sumAll
