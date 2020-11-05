857f9589d91511600cff4ed03b6b868ace2621ca
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 14:36:55 -0800

    Add remote from array exercise solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    if (args === undefined || args.length == 0) return array;
+    // Returns a newly created filtered array
+    return array.filter((element) => {
+        if (args.indexOf(element) == -1) {
+            return element;
+        }
+    });
 }
+
+removeFromArray([1,2,3,4], 3);
 
 module.exports = removeFromArray
