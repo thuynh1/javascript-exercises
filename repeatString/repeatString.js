89bd3c39ed7c0a5fdf0c3c458c2c9a9f8fbe5b65
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 14:36:12 -0800

    Add repeat string exercise solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, amount) {
+    if (str == "") return str;
+    if (amount < 0) return "ERROR";
 
+    let result = "";
+    for (let i = 0; i < amount; i++) {
+        result += str;
+    }
+    return result;
 }
 
 module.exports = repeatString
