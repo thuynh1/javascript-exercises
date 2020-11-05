5d3ab89ac3a3d2a3650d1bbb321f0ce2fa6018fa
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 12:52:11 -0800

    Add reverse string exercise solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Do not use the built in function.
+    // It will fail if your input contains UTF-16 characters (e.g, 𝌆)
+    // return string.split("").reverse().join("");
+    if (string == "") return "";
+    
+    let result = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i];
+    }
+    return result;
 }
 
 module.exports = reverseString
