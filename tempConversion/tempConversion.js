195f7f9b8e81499f2553856850d30d899c9aceac
Tony Huynh <tonyduy092@gmail.com>
2020-11-05 16:12:51 -0800

    Add temp conversion exercise solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let result = (temperature - 32) * 5 / 9;
+  return (result % 1 == 0) ? Number(result.toFixed(0)) : Number(result.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let result = temperature * 9 / 5 + 32;
+  return (result % 1 == 0) ? Number(result.toFixed(0)) : Number(result.toFixed(1));
 }
 
 module.exports = {
