fc728b8905d7b4aad5ad642d5e585df3d1481e23
Tony Huynh <tonyduy092@gmail.com>
2020-11-23 10:44:27 -0800

    Add calculator exercise solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,47 @@
-function add () {
-	
+// Notes: Spread syntax "..." converts args into an Array
+function add(x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract(...args) {
+	if (args == null || args.length === 0) return 0;
+	if (args.length === 1) return args[0];
+	let result = args[0];
+	for (let i = 1; i < args.length; i += 1) {
+		result -= args[i];
+	}
+	return result;
 }
 
-function sum () {
-	
+function sum(arr) {
+	if (arr == null || arr.length === 0) return 0;
+	let result = 0;
+	for (let num of arr) {
+		result += num;
+	}
+	return result;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	if (arr == null || arr.length === 0) return 0;
+	let result = 1;
+	for (let num of arr) {
+		result *= num;
+	}
+	return result;
 }
 
-function power() {
-	
+function power(x, y) {
+	return x ** y;
 }
 
-function factorial() {
-	
+/**
+ * n! = n * (n - 1) * ... * 1
+ */
+function factorial(n) {
+	if (n == null) return 0;
+	if (n === 0) return 1;
+	return n * factorial(n - 1);
 }
 
 module.exports = {
