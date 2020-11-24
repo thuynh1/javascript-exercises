1e5182f6c09dde30fd2404cabf47d53daee21b34
Tony Huynh <tonyduy092@gmail.com>
2020-11-24 15:19:54 -0800

    Add fibonacci exercise solutions

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // We cam also do this with Memoization
+    if (num < 1) return "OOPS";
 
+    let a = 0;
+    let b = 1;
+    let temp;
+    
+    while (num > 0) {
+        temp = a;
+        a = b;
+        b += temp;
+        num -= 1;
+    }
+
+    return a;
 }
 
 module.exports = fibonacci
