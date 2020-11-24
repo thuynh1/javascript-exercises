aa4d5b1fa572f5a821e1e53b6d67fda5a6e55268
Tony Huynh <tonyduy092@gmail.com>
2020-11-24 14:05:23 -0800

    Add palindrome exercise solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    const len = Math.floor(str.length / 2);
+    const regex = /[^a-z]/g;    // Regex to select non-alpha characters (a-z)
+    str = str.toLowerCase().replace(regex, "");
 
+    for (let i = 0; i < len; i += 1) {
+        if (str[i] != str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 module.exports = palindromes
